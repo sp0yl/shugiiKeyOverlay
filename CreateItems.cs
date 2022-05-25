3500f577aa55c4cc9f60323e84eb19b89f5077c5
sparshiexo <sparsheee@gmail.com>
2022-05-25 05:28:08 -0400

    Current progress on new repo

--- CreateItems.cs
+++ CreateItems.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,10 +10,10 @@ namespace KeyOverlay
     public static class CreateItems
     {
         public static readonly Font _font = new Font(Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "Resources",
-            "consolab.ttf")));
+            "Exo2-SemiBoldItalic.ttf")));
         public static RectangleShape CreateBar(RectangleShape square, int outlineThickness, float barSpeed)
         {
-            var rect = new RectangleShape(new Vector2f(square.Size.X + outlineThickness * 2, barSpeed));
+            var rect = new RectangleShape(new Vector2f(square.Size.X + outlineThickness, barSpeed));
             rect.Position = new Vector2f(square.Position.X - outlineThickness,
                 square.Position.Y - square.Size.Y - square.OutlineThickness);
             rect.FillColor = square.FillColor;
@@ -23,19 +23,19 @@ namespace KeyOverlay
         public static List<RectangleShape> CreateKeys(int keyAmount, int outlineThickness, float size, float ratioX, float ratioY,
             int margin, RenderWindow window, Color backgroundColor, Color outlineColor)
         {
-            var yPos = 900 * ratioY;
-            var width = size + outlineThickness * 2;
+            //var yPos = 180;
+            var width = size;
             var keyList = new List<RectangleShape>();
-            var spacing = (window.Size.X - margin * 2 - width * keyAmount) / (keyAmount - 1);
+            var spacing = (window.Size.X - margin * 2 - width * 1);
             for (int i = 0; i < keyAmount; i++)
             {
                 var square = new RectangleShape(new Vector2f(size, size));
-                
+
                 square.FillColor = backgroundColor;
                 square.OutlineColor = outlineColor;
-                square.OutlineThickness = outlineThickness;
-                square.Origin = new Vector2f(0, size);
-                square.Position = new Vector2f(margin +outlineThickness + (width + spacing ) * i , yPos); 
+                square.OutlineThickness = 0;
+                square.Origin = new Vector2f(-60, 120);
+                square.Position = new Vector2f(margin + outlineThickness + (width + spacing) * i, size);
                 keyList.Add(square);
             }
             return keyList;
@@ -44,20 +44,23 @@ namespace KeyOverlay
         public static Text CreateText(string key, RectangleShape square, Color color, bool counter)
         {
             var text = new Text(key, _font);
-            text.CharacterSize = (uint)(50 * square.Size.X / 140);
+            if (counter)
+                text.CharacterSize = text.CharacterSize = (uint)(23 * square.Size.X / 140);
+            else
+                text.CharacterSize = (uint)(50 * square.Size.X / 140);
             text.Style = Text.Styles.Bold;
             text.FillColor = color;
             text.Origin = new Vector2f(text.GetLocalBounds().Width / 2f, 32 * square.Size.X / 140f);
-            if(counter)
+            if (counter)
                 text.Position = new Vector2f(square.GetGlobalBounds().Left + square.OutlineThickness + square.Size.X / 2f,
-                    square.GetGlobalBounds().Top + square.OutlineThickness + square.Size.Y +text.CharacterSize);
+                    square.GetGlobalBounds().Top + square.OutlineThickness + square.Size.Y + text.CharacterSize / 3f - 20);
             else
                 text.Position = new Vector2f(square.GetGlobalBounds().Left + square.OutlineThickness + square.Size.X / 2f,
-                    square.GetGlobalBounds().Top + square.OutlineThickness + square.Size.Y / 2f);
+                    square.GetGlobalBounds().Top + square.OutlineThickness + square.Size.Y / 3f + 5);
 
             return text;
         }
-        
+
         public static Color CreateColor(string s)
         {
             var bytes = s.Split(',').Select(int.Parse).Select(Convert.ToByte).ToArray();
--- Fading.cs
+++ Fading.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.ComponentModel;
 using SFML.Graphics;
 using SFML.System;
@@ -7,16 +7,17 @@ namespace KeyOverlay {
     public static class Fading
     {
         public static List<Sprite> GetBackgroundColorFadingTexture(Color backgroundColor, uint windowWidth,
-            float ratioY) {
-            var sprites = new List<Sprite>();
-            var alpha = 255;
+            float ratioY)
+        {
+            List<Sprite> sprites = new();
+            var alpha = 0;
             var color = backgroundColor;
             for (int i = 0; i < 255; i++)
             {
                 Image img = ratioY>=.5f ? new Image(windowWidth, (uint)(2 * ratioY), color) : new Image(windowWidth, 1, color);
                 var sprite = new Sprite(new Texture(img));
-                sprite.Position = new Vector2f(0, img.Size.Y * i);
                 sprites.Add(sprite);
+                alpha = -i + 235;
                 alpha -= 1;
                 color.A = (byte)alpha;
             }
